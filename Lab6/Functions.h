6fcb54883ee4197afdd57e378b3837f53fbdb44d
AntonNov <56130014+AntonNov@users.noreply.github.com>
2020-05-20 02:45:14 +0300

    Create Functions.h

---
+++ Lab6/Functions.h
@@ -0,0 +1,6 @@
+#pragma once
+void EnterInt(int* _i);
+void EnterNumberOfTrees(int* _i);
+/* Индексация от 0 до number_of_trees-1*/
+void EnterNumberOfTree(int* _i, int number_of_trees);
+void EnterChar(char* _ch);
